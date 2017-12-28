78207e396d08318fad5dcaa55bfba730c3becbb7
yasui321 <sample@sample.com>
2017-12-28 16:47:55 +0000

    fixed order and reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,32 +1,20 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
-  def create_reservation
-    @event = Event.find(params[:event_id])
-    @reservation = current_reservation
-    @reservation.event_id = @event.id
+  def new
+    @reservation = Reservation.new(event_id: params[:event_id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
     @reservation.user_id = current_user.id
-    @reservation.save
-    session[:reservation_id] = @reservation.id
-    redirect_to view_reservation_path
-  end
-
-  def view_reservation
-    @reservation = current_reservation
-  end
-
-  def complete_reservation
-    @reservation = current_reservation
-    @reservation.attributes = reservation_params
-    if @reservation.save(context: :complete_reservation)
+    if @reservation.save
       EventMailer.send_when_complete(current_user).deliver
       redirect_to events_path, notice: '応募が完了しました！'
       @reservation.event.event_people -= @reservation.reservation_people
       @reservation.event.save
-      # binding.pry
-      session[:reservation_id] = nil
     else
-      render :view_reservation
+      render :new
     end
   end
 
@@ -34,7 +22,8 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation)
           .permit(
-            :reservation_name, :reservation_tel, :reservation_people
+            :reservation_name, :reservation_tel, :reservation_people,
+            :event_id, :user_id
           )
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,17 +2,12 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  with_options on: :complete_reservation do |complete|
-    complete.validates :reservation_name, presence: true,
-                                          length: { maximum: 25 }
-    complete.validates :reservation_tel, presence: true,
-                                         format: {
-                                          with: /\A\d+-\d+-\d+\z/
-                                         }
-    complete.validates :reservation_people, presence: true,
-                                            numericality: {
-                                             greater_than: 0,
-                                             less_than: 11
-                                            }
-  end
+  validates :reservation_name, presence: true, length: { maximum: 25 }
+  validates :reservation_tel, presence: true, format: {
+                                                with: /\A\d+-\d+-\d+\z/
+                                              }
+  validates :reservation_people, presence: true, numericality: {
+                                                   greater_than: 0,
+                                                   less_than: 11
+                                                 }
 end
